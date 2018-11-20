041f62c930a21a62fc0d040ff151662a03d1d139
Muayyaduddin <45200916+muayyaduddin@users.noreply.github.com>
2018-11-20 19:28:16 +0700

    Update hello.java
    edit dari github

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello
     public static void main(String[] args){
         System.out.println("Hello world!");
         System.out.println("Kami sedang belajar Git");
+        System.out.println("Edit dari github");
     }
 }
